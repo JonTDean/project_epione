f5f7e3bf963da2b538a5de6c0f4a51db4d11a75e
JonTDean <JonathanTomDean@Gmail.com>
2023-08-28 23:52:56 -0400

    Reconfigured registration circuit and mimc optimizations

--- src/auth/zk_snarks/mimc_hash.rs
+++ src/auth/zk_snarks/mimc_hash.rs
@@ -1,10 +1,6 @@
 use bellman::{ConstraintSystem, SynthesisError, LinearCombination};
 use bls12_381::Scalar;
 
-/// Parameters for MiMC
-const MIMC_ROUNDS: usize = 220;  // Number of rounds
-const MIMC_CONST_SCALAR: Scalar = Scalar::from(42u64);
-
 fn multiply<CS: ConstraintSystem<Scalar>>(
     cs: &mut CS,
     x: &LinearCombination<Scalar>,
@@ -31,10 +27,13 @@ pub fn mimc_round<CS: ConstraintSystem<Scalar>>(
     k: &LinearCombination<Scalar>,
 ) -> Result<LinearCombination<Scalar>, SynthesisError> {
     let mut tmp = x.clone() + k;
-    // !!! Conversion for Combination to Scalar
-    let mimc_const_lc = LinearCombination::zero() + (MIMC_CONST_SCALAR, CS::one());
+    let mimc_const_scalar = Scalar::from(42u64); // Parameters for MiMC
+    let mimc_rounds: usize = 220;  // Number of rounds 
 
-    for _ in 0..MIMC_ROUNDS {
+    // !!! Conversion for Combination to Scalar
+    let mimc_const_lc = LinearCombination::zero() + (mimc_const_scalar, CS::one());
+
+    for _ in 0..mimc_rounds {
         tmp = multiply(cs, &tmp, &tmp)?;
         tmp = tmp.clone() + &mimc_const_lc;
     }
--- src/auth/zk_snarks/registration_circuit.rs
+++ src/auth/zk_snarks/registration_circuit.rs
@@ -1,22 +1,7 @@
-#![allow(unused_imports)]
-use bellman::{Circuit, ConstraintSystem, SynthesisError, LinearCombination, Variable};
+use bellman::{ Circuit, ConstraintSystem, SynthesisError, LinearCombination };
 use bls12_381::Scalar;
 
-struct AuthenticationCircuit {
-    // The known public key (public input).
-    public_key: Option<Scalar>,
-    // The secret private key (private input).
-    private_key: Scalar,
-}
-
-// Note: The actual logic for this circuit will depend on what you're trying to prove 
-// regarding the authentication process.
-impl Circuit<Scalar> for AuthenticationCircuit {
-    fn synthesize<CS: ConstraintSystem<Scalar>>(self, cs: &mut CS) -> Result<(), SynthesisError> {
-        // ... Authentication circuit logic here ...
-        Ok(())
-    }
-}
+use super::mimc_hash::hash_function;
 
 struct RegistrationCircuit {
     // The known hash (public input).
@@ -34,12 +19,13 @@ impl Circuit<Scalar> for RegistrationCircuit {
         // Let's assume we have a `hash_function` that takes a linear combination 
         // and returns the hashed value as another linear combination.
         // This would be a gadget that you implement or use from an existing library.
-        let computed_hash = hash_function(cs, password_var)?;
+        let lc_password = LinearCombination::zero() + password_var;
+        let computed_hash = hash_function(cs, lc_password)?;
 
         // Enforce that the computed hash equals the provided hash
         cs.enforce(
             || "hash(password) = hash",
-            |lc| lc + computed_hash,
+            |lc| lc.clone() + &computed_hash,
             |lc| lc + CS::one(),
             |lc| lc + hash_var,
         );
@@ -47,5 +33,3 @@ impl Circuit<Scalar> for RegistrationCircuit {
         Ok(())
     }
 }
-
-// Note: You'll need to define or import the `hash_function` used in the `RegistrationCircuit`.
